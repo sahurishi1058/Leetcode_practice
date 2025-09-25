7b636447458ea042b78e90de4465fba3764f29eb
sahurishi1058 <sahurick36@gmail.com>
2025-09-26 02:17:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,42 +5,34 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        def twosum(nums1,target1):
-            n=len(nums1)
-            start=0
-            end=n-1
-            ans=[]
-            while start<end:
-                currsum=nums1[start]+nums1[end]
-                if currsum==target1:    
-                    ans.append([nums1[start], nums1[end]])
-                    start += 1
-                    end -= 1
-                    while start < end and nums1[start] == nums1[start - 1]:
-                        start += 1
-                    while start < end and nums1[end] == nums1[end + 1]:
-                        end -= 1
-                elif currsum > target1:
-                    end -= 1
-                else:
-                    start += 1
-            return ans
-
-        nums.sort()
-        n=len(nums)
         ans=[]
-        for i in range(n-3):
-            a=nums[i]
-            if i > 0 and nums[i] == nums[i - 1]:
+        n=len(nums)
+        nums.sort()
+        for i in range(n):
+            if(i>0 and nums[i]==nums[i-1]):
                 continue
-            for j in range(i + 1, n - 2):
-                b = nums[j]
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range(i+1,n):
+                if(j!=i+1 and nums[j]==nums[j-1]):
                     continue
-                t1 = target - a - b
-                k = twosum(nums[j + 1:], t1)
-                for pair in k:
-                    ans.append([a, b] + pair)
+                k=j+1
+                l=n-1
+                while k<l:
+                    sum_tar=nums[i]
+                    sum_tar+=nums[j]
+                    sum_tar+=nums[k]
+                    sum_tar+=nums[l]
+                    if sum_tar==target:
+                        ans.append([nums[i],nums[j],nums[k],nums[l]])
+                        k+=1
+                        l-=1
+                        while (k<l and nums[k]==nums[k+1]):
+                            k+=1
+                        while (k<l and nums[l]==nums[l-1]):
+                            l-=1
+                    elif sum_tar<target:
+                        k+=1
+                    else:
+                        l-=1
         return ans
-
-           
+                        
+        
