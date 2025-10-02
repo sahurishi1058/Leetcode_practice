c02081fd225b2fa76528c21012f1e870df23e932
sahurishi1058 <sahurick36@gmail.com>
2025-10-02 12:02:43 +0530

    Time: 0 ms (100%), Space: 12.5 MB (73.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,13 +10,14 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not list1 or not list2:
+        if not list1 or not list2 :
             return list1 or list2
-        if list1.val <= list2.val:
+        if list1.val<=list2.val:
             list1.next=self.mergeTwoLists(list1.next,list2)
             return list1
         else:
             list2.next=self.mergeTwoLists(list2.next,list1)
-            return list2
+
+        return list2
+
         
-         
