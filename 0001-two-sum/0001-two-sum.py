17d1556b7004d5631f3c90cd188dd44ae84cf5a6
sahurishi1058 <sahurick36@gmail.com>
2025-11-16 16:10:22 +0530

    Time: 0 ms (100%), Space: 13 MB (90.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,15 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        temp=nums.copy()
-        nums.sort()
-        n=len(nums)
-        start=0
-        end=n-1
-        while start<end:
-            currsum=nums[start]+nums[end]
-            if currsum==target:
-                ans=[]
-                for i in range(n):
-                    if nums[start]==temp[i] or temp[i]==nums[end]:
-                        ans.append(i)
-                return ans 
-            elif currsum<target:
-                start+=1
-            else:
-                end-=1
-        return -1
+import copy
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         
+        hashmap={}
+        for i,num in enumerate(nums):
+            complement=target-num
+            if complement in hashmap:
+                return [hashmap[complement],i]
+            hashmap[num]=i
